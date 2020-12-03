4c07a63dd890677afeaa1259abb43e9086d618f9
Mina Rezkalla <minashakeralfy@gmail.com>
2020-12-03 12:45:16 +0100

    add problem 999 of  Available Captures for Rook

---
+++ src/easy/AvailableCapturesforRook999.kt
@@ -0,0 +1,21 @@
+package easy
+
+/*
+On an 8 x 8 chessboard, there is one white rook.  There also may be empty squares, white bishops, and black pawns.  These are given as characters 'R', '.', 'B', and 'p' respectively.
+ Uppercase characters represent white pieces, and lowercase characters represent black pieces.
+
+The rook moves as in the rules of Chess: it chooses one of four cardinal directions (north, east, west, and south),
+ then moves in that direction until it chooses to stop, reaches the edge of the board, or captures an opposite colored pawn by moving to the same square it occupies.  Also, rooks cannot move into the same square as other friendly bishops.
+
+Return the number of pawns the rook can capture in one move.
+
+ Example 1
+Input: [[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".","R",".",".",".","p"],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]]
+Output: 3
+Explanation:
+In this example the rook is able to capture all the pawns.
+ */
+fun numRookCaptures(board: Array<CharArray>): Int {
+
+    return 0
+}
