2eb167b805a6947b7d24d66779f300eedf4a3611
Mina Rezkalla <minashakeralfy@gmail.com>
2020-12-07 13:09:50 +0100

    add problem 1260 Shift 2D Grid [easy]

---
+++ src/easy/Shift2DGrid1260.kt
@@ -0,0 +1,22 @@
+package easy
+
+/*
+Given a 2D grid of size m x n and an integer k. You need to shift the grid k times.
+
+In one shift operation:
+
+Element at grid[i][j] moves to grid[i][j + 1].
+Element at grid[i][n - 1] moves to grid[i + 1][0].
+Element at grid[m - 1][n - 1] moves to grid[0][0].
+Return the 2D grid after applying shift operation k times.
+
+
+Example 1:
+
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+Output: [[9,1,2],[3,4,5],[6,7,8]]
+ */
+
+fun shiftGrid(grid: Array<IntArray>, k: Int): List<List<Int>> {
+return emptyList()
+}
