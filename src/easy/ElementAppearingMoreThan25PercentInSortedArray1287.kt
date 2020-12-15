dee7f942f4b5bd7a0733220c73f6969133ff8521
Mina Rezkalla <minashakeralfy@gmail.com>
2020-12-15 13:46:44 +0100

    add problem #1287 Element Appearing More Than 25 Percent In Sorted Array

---
+++ src/easy/ElementAppearingMoreThan25PercentInSortedArray1287.kt
@@ -0,0 +1,16 @@
+package easy
+
+/*
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time.
+
+Return that integer.
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+ */
+
+fun findSpecialInteger(arr: IntArray): Int {
+return 0
+}
