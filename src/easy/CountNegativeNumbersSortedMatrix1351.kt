e3a84cd681cf04169bcf8f6a90492aa063a7e432
Mina Rezkalla <minashakeralfy@gmail.com>
2020-12-23 14:16:39 +0100

    add problem number 1351 Count Negative Numbers in a Sorted Matrix

---
+++ src/easy/CountNegativeNumbersSortedMatrix1351.kt
@@ -0,0 +1,28 @@
+package easy
+
+/*
+Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise.
+
+Return the number of negative numbers in grid.
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+Example 3:
+
+Input: grid = [[1,-1],[-1,-1]]
+Output: 3
+Example 4:
+
+Input: grid = [[-1]]
+Output: 1
+ */
+
+fun countNegatives(grid: Array<IntArray>): Int {
+return 0
+}
