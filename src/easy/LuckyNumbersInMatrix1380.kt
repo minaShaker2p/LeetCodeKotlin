9d379152ec38e95b8184228632dc5bcf7c261b42
Mina Rezkalla <minashakeralfy@gmail.com>
2020-12-25 17:44:07 +0100

    add a problem number 1380 Lucky Numbers in a Matrix

---
+++ src/easy/LuckyNumbersInMatrix1380.kt
@@ -0,0 +1,25 @@
+package easy
+
+/*
+Given a m * n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+
+A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+
+Example 1:
+
+Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+Output: [15]
+Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column
+Example 2:
+
+Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+Output: [12]
+Explanation: 12 is the only lucky number since it is the minimum in its row and the maximum in its column.
+Example 3:
+
+Input: matrix = [[7,8],[1,2]]
+Output: [7]
+ */
+fun luckyNumbers (matrix: Array<IntArray>): List<Int> {
+ return emptyList()
+}
