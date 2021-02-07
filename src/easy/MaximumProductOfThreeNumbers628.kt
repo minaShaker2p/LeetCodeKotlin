e9c60df0f760d5a68ac5b50dab7f5e0ff5bb6065
Mina Rezkalla <minashakeralfy@gmail.com>
2021-02-07 19:00:11 +0200

    add problem number 628 Maximum Product of Three Numbers

---
+++ src/easy/MaximumProductOfThreeNumbers628.kt
@@ -0,0 +1,22 @@
+package easy
+
+/*
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+ */
+fun maximumProduct(nums: IntArray): Int {
+
+    return 0
+}
