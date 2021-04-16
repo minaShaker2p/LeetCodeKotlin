9660cf54bc8d1e78b2ce6976366d945685888a7a
Mina Rezkalla <minashakeralfy@gmail.com>
2021-04-16 10:25:42 +0200

    add accepted solution for problem  number 509 Fibonncci numbrer

--- src/easy/FibonacciNumber509.kt
+++ src/easy/FibonacciNumber509.kt
@@ -27,5 +27,9 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
  */
 
 fun fib(n: Int): Int {
-return 0
+    if(n==0)
+        return  0
+    if(n==1)
+        return 1
+return fib(n-1) + fib(n-2)
 }
