e9b9b7f192034dd64ff6475689b8df97c69c2247
Mina Rezkalla <minashakeralfy@gmail.com>
2021-04-17 13:46:44 +0200

    add  accepted solution for problem number 1209 Remove All Adjacent Duplicates in String II

--- src/medium/RemoveAllAdjacentDuplicatesStringII1209.kt
+++ src/medium/RemoveAllAdjacentDuplicatesStringII1209.kt
@@ -1,5 +1,8 @@
 package medium
 
+import java.lang.StringBuilder
+import java.util.*
+
 /**
 You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
 
@@ -28,7 +31,43 @@ Input: s = "pbbcggttciiippooaais", k = 2
 Output: "ps"
  */
 
+data class Element(val ch :Char , var count :Int)
 fun removeDuplicates(s: String, k: Int): String {
 
-    return ""
+    val builder = StringBuilder()
+    val stack = Stack<Element>()
+
+    s.forEach {ch->
+        if(stack.isNotEmpty())
+        {
+            val prev = stack.peek()
+            if(ch == prev.ch)
+            {
+                stack.pop()
+                stack.push(Element(ch,prev.count+1))
+            }else
+            {
+                stack.push(Element(ch,1))
+            }
+
+        }else
+        {
+            stack.push(Element(ch,1))
+        }
+
+        if(stack.peek().count ==k)
+            stack.pop()
+    }
+
+    while (stack.isNotEmpty())
+    {
+        repeat(stack.peek().count)
+        {
+            builder.append(stack.peek().ch)
+        }
+        stack.pop()
+    }
+
+
+    return builder.reverse().toString()
 }
