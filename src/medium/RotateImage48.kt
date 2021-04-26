fb86a3bbbc1facf0a6b176828d54c5ed41ee0e50
Mina Rezkalla <minashakeralfy@gmail.com>
2021-04-26 09:14:05 +0200

    add accepted solution for   problem number 48 Rotate Image

--- src/medium/RotateImage48.kt
+++ src/medium/RotateImage48.kt
@@ -27,5 +27,31 @@ Output: [[3,1],[4,2]]
    ***/
 
 fun rotate(matrix: Array<IntArray>): Unit {
+    val n= matrix.size
+
+    // transpose the matrix
+
+    for( i in 0 until n)
+    {
+        for(j in 1 until n)
+        {
+            val temp = matrix[i][j]
+            matrix[i][j]= matrix[j][i]
+            matrix[j][i]=temp
+        }
+    }
+
+
+    // Rotate every row
+
+    for( i in 0 until n/2)
+    {
+        for(j in 0 until n)
+        {
+            val temp = matrix[i][n-j-1]
+            matrix[i][n-j+1]= matrix[i][j]
+            matrix[i][j]=temp
+        }
+    }
 
 }
