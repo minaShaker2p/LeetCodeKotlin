c9e4a47bccf00476c26d6768c6a862e170502367
Mina Rezkalla <minashakeralfy@gmail.com>
2021-04-30 08:37:30 +0200

    [Problem 34]  Find First and Last Position of Element in Sorted Array

---
+++ src/medium/FindFirstAndLastPositionOfElementSortedArray34.kt
@@ -0,0 +1,31 @@
+package medium
+
+/**
+ *
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+Follow up: Could you write an algorithm with O(log n) runtime complexity?
+
+
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+ **/
+
+fun searchRange(nums: IntArray, target: Int): IntArray {
+    val result =IntArray(2){-1}
+return result
+}
