f85ea04b6f7b9bf97b8aab1865897d541c0e80ce
Mina Rezkalla <minashakeralfy@gmail.com>
2021-05-05 08:50:42 +0200

    [Problem 665] Non-decreasing Array

---
+++ src/medium/Non-decreasingArray665.kt
@@ -0,0 +1,24 @@
+package medium
+
+/**
+Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+Example 1:
+
+Input: nums = [4,2,3]
+Output: true
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+
+Input: nums = [4,2,1]
+Output: false
+Explanation: You can't get a non-decreasing array by modify at most one element.
+
+ **/
+
+fun checkPossibility(nums: IntArray): Boolean {
+
+    return false
+}
