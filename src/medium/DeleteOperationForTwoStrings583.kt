8fac95359e654cbc2876f9280df33b3781b9c0ba
Mina Rezkalla <minashakeralfy@gmail.com>
2021-05-08 18:50:42 +0200

    [Problem 583]  Delete Operation for Two Strings

---
+++ src/medium/DeleteOperationForTwoStrings583.kt
@@ -0,0 +1,31 @@
+package medium
+
+/**
+Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+
+
+Example 1:
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Example 2:
+
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+
+
+Constraints:
+
+1 <= word1.length, word2.length <= 500
+word1 and word2 consist of only lowercase English letters.
+
+ **/
+
+fun minDistance(word1: String, word2: String): Int {
+
+    return 0
+}
