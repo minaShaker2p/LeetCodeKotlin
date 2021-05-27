9f5103e0a57f22f23ddb886e6de1d6252d556427
Mina Rezkalla <minashakeralfy@gmail.com>
2021-05-27 22:05:28 +0200

    [Accepted][Problem 318]  Maximum Product of Word Lengths

--- src/medium/MaximumProductOfWordLengths318.kt
+++ src/medium/MaximumProductOfWordLengths318.kt
@@ -32,6 +32,20 @@ words[i] consists only of lowercase English letters.
 * */
 
 fun maxProduct(words: Array<String>): Int {
+    var  max = 0
+     for (i in 0 until words.size-1)
+         for (j in 1 until  words.size)
+             if (!words[i].isCommon(words[j]))
+                 max = Math.max(max,(words[i].length * words[j].length))
+    return max
+}
 
-    return 0
+fun String.isCommon(another:String):Boolean
+{
+    for(i in 0 until  this.length)
+    {
+        if(another.contains(this[i]))
+            return true
+    }
+    return false
 }
