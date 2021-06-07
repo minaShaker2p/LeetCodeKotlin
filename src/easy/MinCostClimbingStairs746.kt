b48c44ff6597d1fab5f55cee852da77a2ac26cd1
Mina Rezkalla <minashakeralfy@gmail.com>
2021-06-07 21:54:45 +0200

    [Problem 746] Min Cost Climbing Stairs

---
+++ src/easy/MinCostClimbingStairs746.kt
@@ -0,0 +1,30 @@
+package easy
+
+/**
+ * You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: Cheapest is: start on cost[1], pay that cost, and go to the top.
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: Cheapest is: start on cost[0], and only step on 1s, skipping cost[3].
+
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+ */
+fun minCostClimbingStairs(cost: IntArray): Int {
+
+    return 0
+}
