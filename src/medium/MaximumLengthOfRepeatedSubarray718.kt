646aee843119b6f7d577e53735e0b6bd9028379c
Mina Rezkalla <minashakeralfy@gmail.com>
2021-07-09 17:40:48 +0200

    [Correct][Problem 718]  Maximum Length of Repeated Subarray

--- src/medium/MaximumLengthOfRepeatedSubarray718.kt
+++ src/medium/MaximumLengthOfRepeatedSubarray718.kt
@@ -21,6 +21,24 @@ Constraints:
  */
 
 fun findLength(nums1: IntArray, nums2: IntArray): Int {
+    val dp = Array(nums1.size+1){ IntArray(nums2.size+1) }
+    var max = 0
+    for(i in 1 .. nums1.size)
+    {
+        for(j in 1 .. nums2.size)
+        {
+            if(nums1[i-1]==nums2[j-1])
+            {
+                dp[i][j]= dp[i-1][j-1] +1
+                max  = Math.max(max,dp[i][j])
+            }else
+            {
+                dp[i][j]=0
+            }
 
-    return 0
+        }
+    }
+
+
+    return max
 }
