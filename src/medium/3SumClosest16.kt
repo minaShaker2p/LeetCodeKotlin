c082769b29a35d0487bc794e9bf3c9eea1e7a422
Mina Rezkalla <minashakeralfy@gmail.com>
2021-07-28 09:12:57 +0200

    [Accepted][Problem 16] 3Sum Closest

--- src/medium/3SumClosest16.kt
+++ src/medium/3SumClosest16.kt
@@ -1,5 +1,7 @@
 package medium
 
+import kotlin.math.abs
+
 /**
  * Share
 Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
@@ -20,6 +22,27 @@ Constraints:
  */
 
 fun threeSumClosest(nums: IntArray, target: Int): Int {
+    val size = nums.size
+    var result = nums[0]+nums[1]+nums[size-1]
 
-    return 0
+    nums.sort()
+
+    for(i in  0 until size-2)
+    {
+        var low = i+1
+        var high = size-1
+        while (low < high)
+        {
+            val  currentSum = nums[i]+nums[low]+nums[high]
+            if(currentSum < target)
+                low++
+            else
+                high--
+
+            if(Math.abs(target-currentSum) < Math.abs(result- target))
+                result= currentSum
+        }
+    }
+
+    return result
 }
