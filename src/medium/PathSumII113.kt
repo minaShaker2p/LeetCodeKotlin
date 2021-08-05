8dbf9a0abd7022270c3a9496939cf0d455282014
Mina Rezkalla <minashakeralfy@gmail.com>
2021-08-05 13:03:54 +0200

    [Problem 113]  Path Sum II

---
+++ src/medium/PathSumII113.kt
@@ -0,0 +1,36 @@
+package medium
+
+/**
+ * Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+
+A leaf is a node with no children.
+
+
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+ */
+class SolutionPathSum {
+    fun pathSum(root: TreeNode?, targetSum: Int): List<List<Int>> {
+
+    }
+}
