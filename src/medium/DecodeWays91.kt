4b87d44e45c9aa714b9d94cc13752aa7f56c4e5b
Mina Rezkalla <minashakeralfy@gmail.com>
2021-08-18 22:19:48 +0200

    [Accepted][problem 91] Decode Ways

--- src/medium/DecodeWays91.kt
+++ src/medium/DecodeWays91.kt
@@ -51,5 +51,19 @@ LeetCode link:
 https://leetcode.com/problems/decode-ways/
  */
 fun numDecodings(s: String): Int {
-return 0
+    val dp = IntArray(s.length+1)
+
+    dp[0]=1
+    dp[1] = if(s[0]=='0') 0 else 1
+    for (i in 2 .. s.length)
+    {
+        val oneDigit = s.substring(i-1,i).toInt()
+        val twoDigit = s.substring(i-2,i).toInt()
+
+        if(oneDigit >=1)
+            dp[i]+=dp[i-1]
+        if(twoDigit in 10 .. 26)
+            dp[i]+=dp[i-2]
+    }
+return dp[s.length]
 }
