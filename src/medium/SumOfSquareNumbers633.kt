0612aa1f68d5bb95f51b8860635b091b0153c201
Mina Rezkalla <minashakeralfy@gmail.com>
2021-08-25 22:31:05 +0200

    [problem 633] Sum of Square Numbers

---
+++ src/medium/SumOfSquareNumbers633.kt
@@ -0,0 +1,37 @@
+package medium
+
+/**
+ * Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+
+
+Example 1:
+
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+Example 2:
+
+Input: c = 3
+Output: false
+Example 3:
+
+Input: c = 4
+Output: true
+Example 4:
+
+Input: c = 2
+Output: true
+Example 5:
+
+Input: c = 1
+Output: true
+
+
+Constraints:
+
+0 <= c <= 231 - 1
+ */
+fun judgeSquareSum(c: Int): Boolean {
+return false
+}
