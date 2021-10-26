787ba484ad480dfd6c027dadbd49dfa4dbf77809
Mina Rezkalla <minashakeralfy@gmail.com>
2021-10-26 20:45:25 +0200

    [Accepted][problem 226] Invert Binary Tree

--- src/easy/InvertBinaryTree226.kt
+++ src/easy/InvertBinaryTree226.kt
@@ -1,5 +1,7 @@
 package easy
 
+import java.util.*
+
 /**
 Given the root of a binary tree, invert the tree, and return its root.
 
@@ -23,4 +25,29 @@ Output: []
 
 fun invertTree(root: TreeNode?): TreeNode? {
 
+    val queue : Queue<TreeNode?> = LinkedList()
+
+    if(root == null) return null
+
+    queue.add(root)
+
+    while (queue.isNotEmpty())
+    {
+        var size = queue.size
+
+        while (size-- > 0)
+        {
+            val current = queue.remove()
+            // swap left and right nodes
+            val temp = current?.left
+            current?.left = current?.right
+            current?.right = temp
+            if(current?.left !=null)
+                queue.add(current.left)
+            if(current?.right !=null)
+                queue.add(current.right)
+        }
+    }
+
+    return root
 }
