70611a169f437e3cbb196c4d9e72fce4d6cd8acb
Mina Rezkalla <minashakeralfy@gmail.com>
2021-11-05 14:10:54 +0200

    [problem 404]  Sum of Left Leaves

---
+++ src/easy/SumOfLeftLeaves404.kt
@@ -0,0 +1,35 @@
+package easy
+
+/**
+ * Given the root of a binary tree, return the sum of all left leaves.
+
+
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+
+Input: root = [1]
+Output: 0
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+ */
+class sumOfLeftLeaves{
+    var sum = 0
+    fun sumOfLeftLeaves(root: TreeNode?): Int {
+
+        return sum
+    }
+
+    fun sumOfLeftLeavesHelper(root: TreeNode?) {
+
+    }
+}
