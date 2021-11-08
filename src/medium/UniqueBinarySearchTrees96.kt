255a56a1942d820dd4f4441efbe9bd4c9e230fe2
Mina Rezkalla <minashakeralfy@gmail.com>
2021-11-08 22:11:05 +0200

    [problem 96] Unique Binary Search Trees

---
+++ src/medium/UniqueBinarySearchTrees96.kt
@@ -0,0 +1,27 @@
+package medium
+
+/**
+ * Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+
+
+Example 1:
+
+
+Input: n = 3
+Output: 5
+Example 2:
+
+Input: n = 1
+Output: 1
+
+
+Constraints:
+
+1 <= n <= 19
+ */
+
+fun numTrees(n: Int): Int {
+
+    return 0
+}
