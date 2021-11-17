bf1ba923787545e6654d0356d36204f4189680bb
Mina Rezkalla <minashakeralfy@gmail.com>
2021-11-17 21:28:21 +0200

    [Accepted][problem 62] Unique Paths

--- src/medium/UniquePaths62.kt
+++ src/medium/UniquePaths62.kt
@@ -33,6 +33,17 @@ Input: m = 3, n = 3
 Output: 6
  */
 fun uniquePaths(m: Int, n: Int): Int {
+    val dp = Array(m){IntArray(n)}
+    for(i in 0 until m)
+        dp[i][0]=1
+
+    for(i in 0 until n)
+        dp[0][i]=1
+
+    for (i in 1 until m)
+        for(j in 1 until n)
+        dp[i][j]= dp[i-1][j]+dp[i][j-1]
 
 
+    return dp[m-1][n-1]
 }
