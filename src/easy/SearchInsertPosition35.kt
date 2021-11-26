0a14c6d8670b35b670bc91eb1d72bc755c6b7370
Mina Rezkalla <minashakeralfy@gmail.com>
2021-11-26 23:29:20 +0200

    [Accepted][problem 35] Search Insert Position

--- src/easy/SearchInsertPosition35.kt
+++ src/easy/SearchInsertPosition35.kt
@@ -37,5 +37,21 @@ nums contains distinct values sorted in ascending order.
 -104 <= target <= 104
  */
 fun searchInsert(nums: IntArray, target: Int): Int {
+    var start = 0
+    var end = nums.size-1
+
+    while (end >= start)
+    {
+        var mid = start + (end-start)/2
+
+        if(nums[mid] == target)
+            return mid
+
+        if (target < nums[mid])
+            end = mid-1
+        else
+            start = mid+1
+    }
+    return end+1
 
 }
