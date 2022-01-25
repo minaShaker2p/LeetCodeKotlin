c5e02ff7c98a1a33fd8bac04fc17ddf42fc26a0d
Mina Rezkalla <minashakeralfy@gmail.com>
2022-01-25 19:58:19 +0200

    [Accepted][problem 941] Valid Mountain Array

--- src/easy/ValidMountainArray941.kt
+++ src/easy/ValidMountainArray941.kt
@@ -30,11 +30,45 @@ Constraints:
 1 <= arr.length <= 104
 0 <= arr[i] <= 104
 
- Problem link :
-https://leetcode.com/problems/valid-mountain-array/
  */
 
 fun validMountainArray(arr: IntArray): Boolean {
+    if(arr.size<3) return false
+
+    // find the index of max value
+
+    var max = Int.MIN_VALUE
+    var maxIndex = -1
+
+    for(i in arr.indices)
+    {
+        if(arr[i]>max)
+        {
+            max= arr[i]
+            maxIndex=i
+        }
+    }
+
+    // check if maxIndex is not first or last item in the array
+
+    if(maxIndex == 0 || maxIndex == arr.size-1)
+        return false
+
+    // check if the array values from 0 until maxIndex increasing
+    // arr[i] < arr[i+1] <arr[i+2] ... arr[maxIndex-1]
+    for(i in 0 until maxIndex-1)
+    {
+        if(arr[i]>=arr[i+1])
+            return false
+    }
+
+    // check if the array values from  maxIndex until to array size -1 decreasing
+    // arr[maxIndex] > arr[maxIndex+1] > ... arr[arr.size-1]
+    for(i in  maxIndex until arr.size-1)
+    {
+        if(arr[i]<=arr[i+1])
+            return false
+    }
 
     return true
 }
