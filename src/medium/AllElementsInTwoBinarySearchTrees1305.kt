398a8d6c920b92a997039b356ea79e4982a5c860
Mina Rezkalla <minashakeralfy@gmail.com>
2022-01-26 20:15:11 +0200

    [problem 1305]  All Elements in Two Binary Search Trees

---
+++ src/medium/AllElementsInTwoBinarySearchTrees1305.kt
@@ -0,0 +1,26 @@
+package medium
+
+/**
+Given two binary search trees root1 and root2, return a list containing all the integers from both trees sorted in ascending order.
+
+Example 1:
+
+
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+Example 2:
+
+
+Input: root1 = [1,null,8], root2 = [8,1]
+Output: [1,1,8,8]
+
+
+Constraints:
+
+The number of nodes in each tree is in the range [0, 5000].
+-105 <= Node.val <= 105
+ */
+
+fun getAllElements(root1: TreeNode?, root2: TreeNode?): List<Int> {
+
+}
