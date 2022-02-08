611b9ae1495099884e969ef8021ccae2b6425f49
Mina Rezkalla <minashakeralfy@gmail.com>
2022-02-08 20:04:49 +0200

    [problem 258]  Add Digits

---
+++ src/easy/AddDigits258.kt
@@ -0,0 +1,29 @@
+package easy
+
+/**
+ * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+
+
+Constraints:
+
+0 <= num <= 231 - 1
+ */
+
+fun addDigits(num: Int): Int {
+return 0
+}
