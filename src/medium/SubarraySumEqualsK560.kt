ebb94439f6dcccdac72347c0ad029223c4c12b05
Mina Rezkalla <minashakeralfy@gmail.com>
2022-02-10 19:23:35 +0200

    [problem 560] Subarray Sum Equals K

---
+++ src/medium/SubarraySumEqualsK560.kt
@@ -0,0 +1,21 @@
+package medium
+
+/**
+ * Given an array of integers nums and an integer k,
+ * return the total number of continuous subarrays whose sum equals to k.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+ */
+fun subarraySum(nums: IntArray, k: Int): Int {
+
+}
