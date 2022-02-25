6c588c7c02e47bb504c6ce0762a392ed8de150c0
Mina Rezkalla <minashakeralfy@gmail.com>
2022-02-25 21:03:19 +0200

    [ACCEPTED][problem 148]   Sort List

--- src/medium/SortList148.kt
+++ src/medium/SortList148.kt
@@ -27,5 +27,56 @@ The number of nodes in the list is in the range [0, 5 * 104].
 -105 <= Node.val <= 105
  */
 fun sortList(head: ListNode?): ListNode? {
-return null
+
+    if(head?.next == null) return head
+
+    var prev:ListNode?=null
+    var slow = head
+    var fast = head
+    while (fast?.next != null)
+    {
+        prev= slow
+        slow=slow?.next
+        fast=fast.next?.next
+    }
+    prev?.next=null
+    val first = sortList(head)
+    val second = sortList(slow)
+return merge(first,second)
+}
+
+fun merge(first: ListNode?,second: ListNode?):ListNode?
+{
+    var temp:ListNode?=ListNode(0)
+    val dummy = temp
+    var head1= first
+    var head2 = second
+    while (head1!=null&&head2!=null)
+    {
+        if(head1.`val`<head2.`val`)
+        {
+            temp?.next=head1
+            head1=head1.next
+        }else
+        {
+            temp?.next=head2
+            head2=head2.next
+        }
+        temp=temp?.next
+    }
+
+    while (head1!=null)
+    {
+        temp?.next=head1
+        head1=head1.next
+        temp=temp?.next
+    }
+
+    while (head2!=null)
+    {
+        temp?.next=head2
+        head2=head2.next
+        temp=temp?.next
+    }
+    return dummy?.next
 }
