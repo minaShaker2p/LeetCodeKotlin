371d18ed9972cda0632e3994bffc7688799a1723
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-02 23:00:35 +0200

    [ACCEPTED][problem 392] Is Subsequence

--- src/easy/IsSubsequence392.kt
+++ src/easy/IsSubsequence392.kt
@@ -27,6 +27,14 @@ s and t consist only of lowercase English letters.
 Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
  */
 fun isSubsequence(s: String, t: String): Boolean {
+    var limit=0
+    for(i in 0 until t.length)
+    {
+        if(limit==s.length)
+            return true
 
-    return false
+        if(s[limit]==t[i])
+            limit++
+    }
+    return limit==s.length
 }
