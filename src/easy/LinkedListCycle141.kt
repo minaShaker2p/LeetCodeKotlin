f4fb4a81664052be6fb28a2c85d1f257cae3471b
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-08 19:22:05 +0200

    [ACCEPTED][Solution 2][problem 141] Linked List Cycle

--- src/easy/LinkedListCycle141.kt
+++ src/easy/LinkedListCycle141.kt
@@ -58,3 +58,16 @@ fun hasCycle(head: ListNode?): Boolean {
     return hasCycle
 
 }
+fun hasCycle2(head: ListNode?): Boolean {
+    if (head?.next == null) return false
+    var slow=head
+    var fast=head
+    while (fast!=null || fast?.next!=null)
+    {
+        slow =slow?.next
+        fast=fast?.next?.next
+        if(slow==fast)
+            return true
+    }
+    return false
+}
