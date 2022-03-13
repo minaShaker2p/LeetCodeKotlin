1da1e27cd1a3ea529b3999d83baf9bdcc8b9930c
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-13 22:52:59 +0200

    [problem 94] Binary Tree Inorder Traversal

---
+++ src/easy/BinaryTreeInorderTraversal.kt
@@ -0,0 +1,30 @@
+package easy
+
+/**
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+fun inorderTraversal(root: TreeNode?): List<Int> {
+return emptyList()
+}
