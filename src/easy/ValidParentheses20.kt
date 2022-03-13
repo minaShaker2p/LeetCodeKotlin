92992fbbfa4bd83c48d5c4027eb4c8eedf79e632
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-13 22:38:32 +0200

    [problem 20]  Valid Parentheses

---
+++ src/easy/ValidParentheses20.kt
@@ -0,0 +1,34 @@
+package easy
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+ */
+
+fun isValid(s: String): Boolean {
+return false
+}
