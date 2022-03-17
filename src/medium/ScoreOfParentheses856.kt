cb98ab71f044e47f800b9b5f101a3c93dab71cae
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-17 18:19:48 +0200

    [problem 856]Score of Parentheses

---
+++ src/medium/ScoreOfParentheses856.kt
@@ -0,0 +1,36 @@
+package medium
+
+/**
+ * Given a balanced parentheses string s, return the score of the string.
+
+The score of a balanced parentheses string is based on the following rule:
+
+"()" has score 1.
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+
+
+Example 1:
+
+Input: s = "()"
+Output: 1
+Example 2:
+
+Input: s = "(())"
+Output: 2
+Example 3:
+
+Input: s = "()()"
+Output: 2
+
+
+Constraints:
+
+2 <= s.length <= 50
+s consists of only '(' and ')'.
+s is a balanced parentheses string.
+ */
+fun scoreOfParentheses(s: String): Int {
+
+    return 0
+}
