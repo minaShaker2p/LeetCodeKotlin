5d44259a4a6a55064dde3d23f211c32f27d8f4b0
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-18 16:48:05 +0200

    [ACCEPTED][problem 316]  Remove Duplicate Letters

--- src/medium/RemoveDuplicateLetters316.kt
+++ src/medium/RemoveDuplicateLetters316.kt
@@ -1,5 +1,8 @@
 package medium
 
+import java.lang.StringBuilder
+import java.util.*
+
 /**
  * Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
 
@@ -21,6 +24,32 @@ Constraints:
 s consists of lowercase English letters.
  */
 fun removeDuplicateLetters(s: String): String {
+    val stack= Stack<Int>()
+    val lastIndex=IntArray(26)
+    val seen=BooleanArray(26)
+
+    for(i in s.indices)
+        lastIndex[s[i]-'a']=i
+
+    for (i in s.indices)
+    {
+        val c= s[i]-'a'
+
+        if(seen[c])continue
+        while (stack.isNotEmpty()&&stack.peek()>c&&i<lastIndex[stack.peek()])
+        {
+            seen[stack.pop()]=false
+        }
+        stack.push(c)
+        seen[c]=true
+    }
+
+    val result=StringBuilder()
+    while (stack.isNotEmpty())
+    {
+        result.insert(0,('a'.plus(stack.pop())))
+    }
 
 
+return result.toString()
 }
