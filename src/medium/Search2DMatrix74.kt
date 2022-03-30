efa35dc2df609c6551b71ff7c848e55eb85e4a38
Mina Rezkalla <minashakeralfy@gmail.com>
2022-03-30 14:48:46 +0200

    [problem 74]  Search a 2D Matrix

---
+++ src/medium/Search2DMatrix74.kt
@@ -0,0 +1,31 @@
+package medium
+
+/**
+ * Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+ */
+fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+return false
+}
