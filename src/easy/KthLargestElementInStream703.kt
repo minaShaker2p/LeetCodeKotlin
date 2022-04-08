0690170edb67d3f9c2ac4a257df816e8c81db407
Mina Rezkalla <minashakeralfy@gmail.com>
2022-04-08 23:56:29 +0200

    [ACCEPTED][problem 703] Kth Largest Element in a Stream

--- src/easy/KthLargestElementInStream703.kt
+++ src/easy/KthLargestElementInStream703.kt
@@ -1,9 +1,25 @@
 package easy
 
 class KthLargest(k: Int, nums: IntArray) {
+    val list=nums.toMutableList()
+    val limit=k
 
     fun add(`val`: Int): Int {
-        return 0
+        list.add(`val`)
+        list.sort()
+        val size=list.size
+        var i=1
+        var largest=list[size-i]
+        while (i<=limit)
+        {
+            if(largest>list[size-i])
+            {
+                largest=list[size-i]
+            }
+            i++
+
+        }
+        return largest
     }
 
 }
