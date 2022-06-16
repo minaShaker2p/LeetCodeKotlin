25b30215597b31a5f5adaf53e5e504ac5d5ea9ff
Mina Rezkalla <minashakeralfy@gmail.com>
2022-06-16 22:30:50 +0200

    [ACCEPTED][Problem 5]Longest Palindromic Substring

--- src/medium/LongestPalindromicSubstring5.kt
+++ src/medium/LongestPalindromicSubstring5.kt
@@ -23,5 +23,30 @@ s consist of only digits and English letters.
  */
 
 fun longestPalindrome(s: String): String {
+    var start=0
+    var end=0
+    for(i in s.indices)
+    {
+        val len=Math.max(expand(s,i,i), expand(s,i,i+1))
+        if (len>end-start)
+        {
+            start= i-(len-1)/2
+            end = i+len/2
+        }
+    }
+    return s.substring(start,end+1)
+
+}
+
+fun expand(s:String,left:Int,right:Int):Int
+{
+    var l=left
+    var r=right
+    while (l>=0 && r<s.length&&s[l]==s[r])
+    {
+        l--
+        r++
+    }
+    return r-l-1
 
 }
