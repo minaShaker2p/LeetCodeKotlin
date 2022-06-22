4931b22a02ba5219bfbd972966de6ef2f7a0264c
Mina Rezkalla <minashakeralfy@gmail.com>
2022-06-22 18:52:09 +0200

    [Problem 215] Kth Largest Element in an Array

---
+++ src/medium/KthLargestElementInAnArray215.kt
@@ -0,0 +1,27 @@
+package medium
+
+/**
+ * Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+
+Constraints:
+
+1 <= k <= nums.length <= 104
+-104 <= nums[i] <= 104
+ */
+fun findKthLargest(nums: IntArray, k: Int): Int {
+return 0
+}
