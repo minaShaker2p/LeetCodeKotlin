8f88aecdf8ba714654916bc1a11982f8ca53702a
Mina Rezkalla <minashakeralfy@gmail.com>
2022-07-11 19:03:42 +0200

    [ACCEPTED][Problem 199] Binary Tree Right Side View

--- src/medium/BinaryTreeRightSideView199.kt
+++ src/medium/BinaryTreeRightSideView199.kt
@@ -1,5 +1,7 @@
 package medium
 
+import java.util.*
+
 /**
  * Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
 
@@ -26,5 +28,25 @@ The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
  */
 fun rightSideView(root: TreeNode?): List<Int> {
+    if(root==null) return emptyList()
+ val visableList= mutableListOf<Int>()
+    val queue :Queue<TreeNode> =LinkedList<TreeNode>()
+    queue.add(root)
+    while (queue.isNotEmpty())
+    {
+        val size= queue.size
+        for(i in 0 until size)
+        {
+            val current= queue.poll()
+            if(i==size-1)
+                visableList.add(current.`val`)
+            if(current?.left!=null)
+                queue.add(current.left)
 
+            if(current?.right!=null)
+                queue.add(current.right)
+
+        }
+    }
+    return visableList
 }
