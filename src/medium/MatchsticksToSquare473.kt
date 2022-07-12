42c005b65142bcab2b68c7ed59a909a14a3c0249
Mina Rezkalla <minashakeralfy@gmail.com>
2022-07-12 20:25:47 +0200

    [Problem 473] Matchsticks to Square

---
+++ src/medium/MatchsticksToSquare473.kt
@@ -0,0 +1,24 @@
+package medium
+
+/**
+ * You are given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick. You want to use all the matchsticks to make one square. You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
+
+Return true if you can make this square and false otherwise.
+
+
+
+Example 1:
+
+
+Input: matchsticks = [1,1,2,2,2]
+Output: true
+Explanation: You can form a square with length 2, one side of the square came two sticks with length 1.
+Example 2:
+
+Input: matchsticks = [3,3,3,3,4]
+Output: false
+Explanation: You cannot find a way to form a square with all the matchsticks.
+ */
+fun makesquare(matchsticks: IntArray): Boolean {
+return false
+}
