418b6ce4f7720e3e5b3bc243575d079f880b8236
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-02 18:52:26 +0200

    [ACCEPTED][Problem 433] Minimum Genetic Mutation

--- src/medium/MinimumGeneticMutation433.kt
+++ src/medium/MinimumGeneticMutation433.kt
@@ -1,5 +1,7 @@
 package medium
 
+import java.util.*
+
 /**A gene string can be represented by an 8-character long string, with choices from 'A', 'C', 'G', and 'T'.
 
 Suppose we need to investigate a mutation from a gene string start to a gene string end where one mutation is defined as one single character changed in the gene string.
@@ -36,5 +38,30 @@ bank[i].length == 8
 start, end, and bank[i] consist of only the characters ['A', 'C', 'G', 'T'].
  */
 fun minMutation(start: String, end: String, bank: Array<String>): Int {
+    val queue : Queue<String> = LinkedList<String>()
+    val seen = mutableSetOf<String>()
+    queue.add(start)
+    seen.add(start)
+    var steps = 0
+    val chars = charArrayOf('A','C', 'G', 'T')
+    while (queue.isNotEmpty()) {
+        val size = queue.size
+        for (i in 0 until size) {
+            val node = queue.remove()
+            if (node == end)
+                return steps
+
+            for (char in chars) {
+                for (j in node.indices) {
+                    val neighbor = node.substring(0, j) + char + node.substring(j + 1)
+                    if (!seen.contains(neighbor) && bank.indexOf(neighbor) != -1) {
+                        queue.add(neighbor)
+                        seen.add(neighbor)
+                    }
+                }
+            }
+        }
+        steps++
+    }
     return -1
 }
