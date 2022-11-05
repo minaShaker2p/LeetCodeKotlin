3d10c107c3d85b876f7d3c5cb7e0236f649ce255
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-06 00:51:01 +0200

    [ACCEPTED][Problem 345] Reverse Vowels of a String

--- src/easy/ReverseVowelsOfString345.kt
+++ src/easy/ReverseVowelsOfString345.kt
@@ -23,4 +23,30 @@ Constraints:
 s consist of printable ASCII characters.
  */
 fun reverseVowels(s: String): String {
+    val reversedWord = CharArray(s.length)
+    var start = 0
+    var end = s.length-1
+    while (start<=end)
+    {
+        if(s[start].isVowel())
+        {
+            while (!s[end].isVowel())
+            {
+                reversedWord[end]=s[end]
+                end--
+            }
+            val startChar = s[start]
+            val endChar = s[end]
+            reversedWord[start]=endChar
+            reversedWord[end]=startChar
+            start++
+            end--
+        }else
+        {
+            reversedWord[start]=s[start]
+            start++
+        }
+    }
+    return reversedWord.joinToString("")
 }
+fun Char.isVowel()= charArrayOf('a', 'e', 'i', 'o',  'u').contains(this.toLowerCase())
