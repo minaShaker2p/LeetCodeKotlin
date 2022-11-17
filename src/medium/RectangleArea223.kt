e5d981248bf35ba099302e9fb7ab6e9b54aad8b0
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-17 12:39:50 +0200

    [Problem 223] Rectangle Area

---
+++ src/medium/RectangleArea223.kt
@@ -0,0 +1,32 @@
+package medium
+
+/**
+ * Given the coordinates of two rectilinear rectangles in a 2D plane, return the total area covered by the two rectangles.
+
+The first rectangle is defined by its bottom-left corner (ax1, ay1) and its top-right corner (ax2, ay2).
+
+The second rectangle is defined by its bottom-left corner (bx1, by1) and its top-right corner (bx2, by2).
+
+
+
+Example 1:
+
+Rectangle Area
+Input: ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
+Output: 45
+Example 2:
+
+Input: ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2, by1 = -2, bx2 = 2, by2 = 2
+Output: 16
+
+
+Constraints:
+
+-104 <= ax1 <= ax2 <= 104
+-104 <= ay1 <= ay2 <= 104
+-104 <= bx1 <= bx2 <= 104
+-104 <= by1 <= by2 <= 104
+ */
+fun computeArea(ax1: Int, ay1: Int, ax2: Int, ay2: Int, bx1: Int, by1: Int, bx2: Int, by2: Int): Int {
+
+}
