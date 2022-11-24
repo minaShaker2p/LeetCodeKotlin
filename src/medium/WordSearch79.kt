49900d94ef1ea33806863d5dbc30bf5be022c4c5
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-24 13:57:15 +0200

    [ACCEPTED][Problem 79]  Word Search

--- src/medium/WordSearch79.kt
+++ src/medium/WordSearch79.kt
@@ -35,4 +35,25 @@ board and word consists of only lowercase and uppercase English letters.
  */
 
 fun exist(board: Array<CharArray>, word: String): Boolean {
+    for (i in board.indices) {
+        for (j in board[0].indices) {
+            if (board[i][j] == word[0] && dfs(board, i, j, 0, word))
+                return true
+        }
+    }
+    return false
+}
+
+fun dfs(board: Array<CharArray>, currRow: Int, currCol: Int, count: Int, word: String): Boolean {
+    if (count == word.length) return true
+    if (currRow < 0 || currRow >= board.size || currCol < 0 || currCol >= board[0].size || board[currRow][currCol] != word[count])
+        return false
+    val temp = board[currRow][currCol]
+    board[currRow][currCol]=' '
+    val found = dfs(board,currRow+1,currCol,count+1,word)
+            ||  dfs(board,currRow-1,currCol,count+1,word)
+            ||   dfs(board,currRow,currCol+1,count+1,word)
+            ||  dfs(board,currRow,currCol-1,count+1,word)
+    board[currRow][currCol]=temp
+    return found
 }
