002660e814c0d492c6b7266cdc10c5f529696ea6
Mina Rezkalla <minashakeralfy@gmail.com>
2024-09-24 18:17:51 +0200

    [ACCEPTED][Problem 3043] Find the Length of the Longest Common Prefix

--- src/medium/FindTheLengthOfTheLongestCommonPrefix3043.kt
+++ src/medium/FindTheLengthOfTheLongestCommonPrefix3043.kt
@@ -1,5 +1,7 @@
 package medium
 
+import kotlin.math.log10
+
 object FindTheLengthOfTheLongestCommonPrefix3043 {
     fun longestCommonPrefix(arr1: IntArray, arr2: IntArray): Int {
         // Store all prefixes from arr1
@@ -11,12 +13,15 @@ object FindTheLengthOfTheLongestCommonPrefix3043 {
         // Iterate over each val in arr2
         arr2.forEach { num ->
             var currNum = num
+            // While currNum is not in arr1Prefixes and currNum is greater than 0
+            // Then reduce the currNum by removing the last digit
             while (arr1Prefixes.contains(currNum).not() && currNum > 0) {
                 currNum /= 10
             }
-
+            // If currentNum is greater than o and matching the prefix is found
+            // Update longestPrefix to the maximum of its current value and the length of the matched prefix (calculating using log10(val) +1)
             if (arr1Prefixes.contains(currNum))
-                longestPrefix = longestPrefix.coerceAtLeast(currNum.toString().length)
+                longestPrefix = longestPrefix.coerceAtLeast(log10(currNum.toDouble()).plus(1).toInt())
         }
 
         return longestPrefix
