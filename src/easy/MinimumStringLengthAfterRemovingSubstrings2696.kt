ac70e7413da245d7b7e928dff6f0d6fd7a4feba0
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-07 09:39:27 +0200

    [ACCEPTED][Problem 2696][SOLUTION 2] Minimum String Length After Removing Substrings

--- src/easy/MinimumStringLengthAfterRemovingSubstrings2696.kt
+++ src/easy/MinimumStringLengthAfterRemovingSubstrings2696.kt
@@ -18,4 +18,25 @@ object MinimumStringLengthAfterRemovingSubstrings2696 {
 
         return characterStack.size
     }
+
+    /**
+     * String Replace
+     */
+    fun minLengthSolution2(s: String): Int {
+       var string = s
+
+        // Continue processing while "AB" or "CD" substrings exist
+        while (string.contains("AB") || string.contains("CD")) {
+            if (string.contains("AB"))
+            {
+                // Remove the occurrences of "AB"
+                string = string.replace("AB","")
+            }else if(string.contains("CD"))
+            {
+                // Remove the occurrences of "CD"
+                string = string.replace("CD","")
+            }
+        }
+        return string.length
+    }
 }
