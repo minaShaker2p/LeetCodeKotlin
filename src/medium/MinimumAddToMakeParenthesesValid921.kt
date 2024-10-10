a0aefa094087a9805e0cc4ba3b4bc5a413dfaa60
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-10 09:09:14 +0200

    [README][Problem 921][Solution 2] Minimum Add to Make Parentheses Valid

--- src/medium/MinimumAddToMakeParenthesesValid921.kt
+++ src/medium/MinimumAddToMakeParenthesesValid921.kt
@@ -21,4 +21,27 @@ object MinimumAddToMakeParenthesesValid921 {
         }
         return stack.size
     }
+
+    fun minAddToMakeValidSolution2(s: String): Int {
+        // initialize openBrackets to track unmatched open brackets
+        var openBrackets = 0
+        var minAddRequired = 0
+
+        s.forEach { char ->
+            if (char == '(')
+                openBrackets++
+            else {
+                // If an open bracket exists, match it with the closing one
+                // If not, we need to add an open bracket
+                if (openBrackets > 0) {
+                    openBrackets--
+                } else {
+                    minAddRequired++
+                }
+            }
+        }
+
+        // Add the remaining open brackets as closing brackets would be required
+        return minAddRequired + openBrackets
+    }
 }
