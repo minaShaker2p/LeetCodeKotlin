98b6db3641df1e7ce6fba13cf620dcaf71b48b4b
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-10 09:25:11 +0200

    [Time Limit Exceeded][Problem 962] Maximum Width Ramp

--- src/medium/MaximumWidthRamp962.kt
+++ src/medium/MaximumWidthRamp962.kt
@@ -2,13 +2,11 @@ package medium
 
 object MaximumWidthRamp962 {
     fun maxWidthRamp(nums: IntArray): Int {
-        var maxRamp = Int.MIN_VALUE
-        for(i in 0 until nums.size-1)
-        {
-            for(j in i+1 until  nums.size)
-            {
-                if(nums[i]<nums[j])
-                    maxRamp= maxRamp.coerceAtLeast(j - i)
+        var maxRamp = 0
+        for (i in 0 until nums.size - 1) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] <= nums[j])
+                    maxRamp = maxRamp.coerceAtLeast(j - i)
             }
         }
         return maxRamp
