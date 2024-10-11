ad08d8280909f5b1d4b55ac8e655d4671db951e7
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-11 10:18:58 +0200

    [NOT ACCEPTED][Problem 962] Maximum Width Ramp

--- src/medium/MaximumWidthRamp962.kt
+++ src/medium/MaximumWidthRamp962.kt
@@ -1,6 +1,7 @@
 package medium
 
 object MaximumWidthRamp962 {
+    // Approach 1: Brute Force (Time Limit Exceeded)
     fun maxWidthRamp(nums: IntArray): Int {
         var maxRamp = 0
         for (i in 0 until nums.size - 1) {
@@ -11,4 +12,35 @@ object MaximumWidthRamp962 {
         }
         return maxRamp
     }
+
+    // Approach 2: Sorting
+    fun maxWidthRampSolution2(nums: IntArray): Int {
+        val n = nums.size
+        val indices = IntArray(n)
+
+        // Initialize the array with indices
+        for (i in 0 until n) {
+            indices[i] = i
+        }
+
+        // Sort indices based on corresponding values in nums and ensure stability
+       val sortedIndices= indices.sortedWith(Comparator<Int> { a, b ->
+            if (nums[a] != nums[b])
+                nums[a] - nums[b]
+            else
+                a - b
+        })
+
+        var minIndex = n // Minimum index encountered so far
+        var maxWidth = 0
+
+        // Calculate maximum width ramp
+        for(i in sortedIndices)
+        {
+            maxWidth = maxWidth.coerceAtLeast(i - minIndex)
+            minIndex = minIndex.coerceAtMost(i)
+        }
+        return maxWidth
+    }
+
 }
