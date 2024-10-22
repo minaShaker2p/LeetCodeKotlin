3fdd5c66d47f240e8f4987e15d63ec99c2d328c9
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-22 22:29:23 +0200

    [README][Problem 2583] [SOLUTION 2] Kth Largest Sum in a Binary Tree

--- src/medium/KthLargestSumInBinaryTree2583.kt
+++ src/medium/KthLargestSumInBinaryTree2583.kt
@@ -1,16 +1,16 @@
 package medium
 
+import java.util.Collections
 import java.util.LinkedList
+import java.util.PriorityQueue
 import java.util.Queue
 
 object KthLargestSumInBinaryTree2583 {
     fun kthLargestLevelSum(root: TreeNode?, k: Int): Long {
-
         val queue: Queue<TreeNode> = LinkedList()
         queue.add(root)
 
         val levelSumList = mutableListOf<Long>()
-        levelSumList.add(root?.`val`?.toLong()?:0)
 
         while (queue.isNotEmpty()) {
             val size = queue.size
@@ -30,5 +30,41 @@ object KthLargestSumInBinaryTree2583 {
 
         return if (k < levelSumList.size) levelSumList[k] else -1
 
+    }
+  /**
+   * Approach 1: Level Order Traversal + Max Heap
+   *
+   */
+    fun kthLargestLevelSumSolution2(root: TreeNode?, k: Int): Long {
+        // Max heap
+        val pq: PriorityQueue<Long> = PriorityQueue(Collections.reverseOrder())
+
+        val bfsQueue: Queue<TreeNode> = LinkedList()
+        bfsQueue.add(root)
+
+        while (bfsQueue.isNotEmpty()) {
+            // Level order traversal
+            val size = bfsQueue.size
+            var sumOfLevel = 0L
+            for (i in 0 until size) {
+                val curr = bfsQueue.remove()
+                sumOfLevel += curr?.`val` ?: 0
+                // Add left child
+                if (curr.left != null)
+                    bfsQueue.add(curr.left)
+
+                // Add right child
+                if (curr.right != null)
+                    bfsQueue.add(curr.right)
+            }
+            pq.add(sumOfLevel)
+        }
+
+        if (pq.size < k) return -1
+        for (i in 0 until k - 1) {
+            pq.remove()
+        }
+
+        return pq.peek()
     }
 }
