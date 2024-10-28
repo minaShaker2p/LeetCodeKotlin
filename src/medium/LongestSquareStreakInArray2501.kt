35d18562de9698b1ad48874ba2ae63c5f7f2beb1
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-28 11:38:24 +0100

    [NOT ACCEPTED][Problem 2501] Longest Square Streak in an Array

--- src/medium/LongestSquareStreakInArray2501.kt
+++ src/medium/LongestSquareStreakInArray2501.kt
@@ -2,6 +2,35 @@ package medium
 
 object LongestSquareStreakInArray2501 {
     fun longestSquareStreak(nums: IntArray): Int {
-     return 0
+        val setOfNums = nums.toSet()
+        val setOSquares = mutableSetOf<Int>()
+        for (num in nums) {
+            val squareOfNum = num * num
+            if (setOfNums.contains(squareOfNum)) {
+                setOSquares.add(num)
+                setOSquares.add(squareOfNum)
+            }
+        }
+
+        // sort the set of squares
+        val sortedSetOfSquares = setOSquares.sorted()
+
+        var longestSquareStreak = 0
+        var streak = 0
+
+        for(i in 1 until sortedSetOfSquares.size)
+        {
+            if((sortedSetOfSquares[i-1] * sortedSetOfSquares[i-1])  != sortedSetOfSquares[i])
+            {
+                streak=0
+            }else
+            {
+                streak++
+            }
+            longestSquareStreak =  Math.max(longestSquareStreak,streak)
+        }
+        if(longestSquareStreak == 0) return -1
+
+        return longestSquareStreak
     }
 }
