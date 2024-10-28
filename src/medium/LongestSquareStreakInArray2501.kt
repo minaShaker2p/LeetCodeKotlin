ebfe04b73d1a65030b50024e79b1839c56ac8988
Mina Rezkalla <minashakeralfy@gmail.com>
2024-10-28 12:00:09 +0100

    [ACCEPTED][Problem 2501] Longest Square Streak in an Array

--- src/medium/LongestSquareStreakInArray2501.kt
+++ src/medium/LongestSquareStreakInArray2501.kt
@@ -2,35 +2,25 @@ package medium
 
 object LongestSquareStreakInArray2501 {
     fun longestSquareStreak(nums: IntArray): Int {
-        val setOfNums = nums.toSet()
-        val setOSquares = mutableSetOf<Int>()
-        for (num in nums) {
-            val squareOfNum = num * num
-            if (setOfNums.contains(squareOfNum)) {
-                setOSquares.add(num)
-                setOSquares.add(squareOfNum)
+        // Create a unique set of numbers
+        val uniqueNumbers = mutableSetOf<Int>()
+        nums.forEach { uniqueNumbers.add(it) }
+
+        var longestStreak = 0
+
+        for (startNumber in nums) {
+            var currentStreak = 0
+            var current = startNumber
+
+            while (uniqueNumbers.contains(current)) {
+                currentStreak++
+
+                if (current * current > 1e5) break
+
+                current *= current
             }
+            longestStreak = longestStreak.coerceAtLeast(currentStreak)
         }
-
-        // sort the set of squares
-        val sortedSetOfSquares = setOSquares.sorted()
-
-        var longestSquareStreak = 0
-        var streak = 0
-
-        for(i in 1 until sortedSetOfSquares.size)
-        {
-            if((sortedSetOfSquares[i-1] * sortedSetOfSquares[i-1])  != sortedSetOfSquares[i])
-            {
-                streak=0
-            }else
-            {
-                streak++
-            }
-            longestSquareStreak =  Math.max(longestSquareStreak,streak)
-        }
-        if(longestSquareStreak == 0) return -1
-
-        return longestSquareStreak
+        return if (longestStreak < 2) -1 else longestStreak
     }
 }
