f186f31d1ed5b1b3d46919d9c510833b48a257d5
Mina Rezkalla <minashakeralfy@gmail.com>
2024-11-04 09:32:26 +0100

    [ACCEPTED][Problem 3163] String Compression III

--- src/Solution.kt
+++ src/Solution.kt
@@ -2,7 +2,7 @@ import easy.*
 import medium.*
 
 fun main(args: Array<String>) {
-val skill = intArrayOf(3,2,5,1,3,4)
-   DividePlayersIntoTeamsOfEqualSkill2491.dividePlayers(skill)
+
+    println(StringCompressionIII3163.compressedString("aaaaaaaaaaaaaabb"))
 
 }
--- src/medium/StringCompressionIII3163.kt
+++ src/medium/StringCompressionIII3163.kt
@@ -2,6 +2,24 @@ package medium
 
 object StringCompressionIII3163 {
     fun compressedString(word: String): String {
+        val compressedString = StringBuilder()
+        val runString = StringBuilder()
+        for (char in word) {
 
+            if (runString.isEmpty() || (runString.last() == char && runString.length<9)) {
+                runString.append(char)
+            } else {
+                compressedString.append(runString.length)
+                compressedString.append(runString.last())
+                runString.clear()
+                runString.append(char)
+            }
+        }
+        if(runString.isNotEmpty())
+        {
+            compressedString.append(runString.length)
+            compressedString.append(runString.last())
+        }
+        return compressedString.toString()
     }
 }
