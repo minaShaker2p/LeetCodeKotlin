b108f5a5f9e609e3a78ba02d58282222551599d8
Mina Rezkalla <minashakeralfy@gmail.com>
2024-11-07 09:36:19 +0100

    [ACCEPTED][SOLUTION 2][Problem 3163] String Compression III

--- src/medium/StringCompressionIII3163.kt
+++ src/medium/StringCompressionIII3163.kt
@@ -6,7 +6,7 @@ object StringCompressionIII3163 {
         val runString = StringBuilder()
         for (char in word) {
 
-            if (runString.isEmpty() || (runString.last() == char && runString.length<9)) {
+            if (runString.isEmpty() || (runString.last() == char && runString.length < 9)) {
                 runString.append(char)
             } else {
                 compressedString.append(runString.length)
@@ -15,10 +15,34 @@ object StringCompressionIII3163 {
                 runString.append(char)
             }
         }
-        if(runString.isNotEmpty())
-        {
+        if (runString.isNotEmpty()) {
             compressedString.append(runString.length)
             compressedString.append(runString.last())
+        }
+        return compressedString.toString()
+    }
+
+    fun compressedStringSolution2(word: String): String {
+        val compressedString = StringBuilder()
+
+        // pos tracks our position in the input string
+        var pos = 0
+
+        // process until we reach end of string
+        while (pos < word.length) {
+            var consecutiveCount = 0
+            val currentChar = word[pos]
+
+            // count consecutive occurrence (maximum 9)
+            while (pos < word.length && consecutiveCount < 9 &&
+                word[pos] == currentChar
+            ) {
+                consecutiveCount++
+                pos++
+            }
+
+            // Append count followed by character to result
+            compressedString.append(consecutiveCount).append(currentChar)
         }
         return compressedString.toString()
     }
