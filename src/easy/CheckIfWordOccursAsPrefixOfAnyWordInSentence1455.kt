c74a97979bf1c1183f7b450d2855984f50078d5a
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-02 11:30:00 +0100

    [ACCEPTED][Problem 1455] Check If a Word Occurs As a Prefix of Any Word in a Sentencer

--- src/easy/CheckIfWordOccursAsPrefixOfAnyWordInSentence1455.kt
+++ src/easy/CheckIfWordOccursAsPrefixOfAnyWordInSentence1455.kt
@@ -1,7 +1,29 @@
 package easy
 
+/**
+ * Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+ *
+ * Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+ *
+ * A prefix of a string s is any leading contiguous substring of s.
+ *
+ * Example 1:
+ *
+ * Input: sentence = "i love eating burger", searchWord = "burg"
+ * Output: 4
+ * Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+ *
+ */
+
 object CheckIfWordOccursAsPrefixOfAnyWordInSentence1455 {
     fun isPrefixOfWord(sentence: String, searchWord: String): Int {
+        val words = sentence.split(" ")
+
+        for (i in words.indices) {
+            if (words[i].startsWith(searchWord, ignoreCase = true)) {
+                return i+1
+            }
+        }
 
         return -1
     }
