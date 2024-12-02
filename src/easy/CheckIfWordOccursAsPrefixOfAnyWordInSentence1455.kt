1f84501d102c2cf995d057d7e73f13659d117437
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-02 11:58:42 +0100

    [SOLUTION2][Problem 1455] Check If a Word Occurs As a Prefix of Any Word in a Sentence

--- src/easy/CheckIfWordOccursAsPrefixOfAnyWordInSentence1455.kt
+++ src/easy/CheckIfWordOccursAsPrefixOfAnyWordInSentence1455.kt
@@ -21,10 +21,53 @@ object CheckIfWordOccursAsPrefixOfAnyWordInSentence1455 {
 
         for (i in words.indices) {
             if (words[i].startsWith(searchWord, ignoreCase = true)) {
-                return i+1
+                return i + 1
             }
         }
 
+        return -1
+    }
+
+    fun isPrefixOfWordSolution2(sentence: String, searchWord: String): Int {
+        // List to store the words from the sentence
+        val wordsList = mutableListOf<String>()
+
+        // StringBuilder to build the current word
+        val currentWord = StringBuilder()
+
+        // Iterate through each character in the sentence
+        for (char in sentence) {
+            if (char != ' ') {
+                // Append the character to the current word
+                currentWord.append(char)
+            } else {
+                // If we encounter a space, add the current word to the list
+                if (currentWord.isNotEmpty()) {
+                    wordsList.add(currentWord.toString())
+                    currentWord.clear()
+                }
+            }
+        }
+
+        // Add the last word if the sentence doesn't end with space
+        if (currentWord.isNotEmpty())
+            wordsList.add(currentWord.toString())
+
+        // Iterate through the list of words to find the prefix match
+        for (wordIndex in wordsList.indices) {
+            val word = wordsList[wordIndex]
+            if (word.length >= searchWord.length) {
+                var isMatch = true
+                for (charIndex in searchWord.indices) {
+                    if (word[charIndex] != searchWord[charIndex]) {
+                        isMatch = false
+                        break
+                    }
+                }
+                if(isMatch)
+                    return wordIndex+1
+            }
+        }
         return -1
     }
 }
