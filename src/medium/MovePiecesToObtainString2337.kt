a84b87cd5531d557bf69eacbedfac7db3ba08498
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-05 09:42:13 +0100

    [NOT ACCEPTED][Problem 2337] Move Pieces to Obtain a String

--- src/medium/MovePiecesToObtainString2337.kt
+++ src/medium/MovePiecesToObtainString2337.kt
@@ -1,7 +1,32 @@
 package medium
+
+import java.util.*
+
 object MovePiecesToObtainString2337 {
     fun canChange(start: String, target: String): Boolean {
+        val startQueue: Queue<Char> = LinkedList()
+        val targetQueue: Queue<Char> = LinkedList()
 
-        return false
+        for (char in start) {
+            if (char != '_')
+                startQueue.add(char)
+        }
+
+        for (char in target) {
+            if (char != '_')
+                targetQueue.add(char)
+        }
+
+        if(startQueue.size != targetQueue.size) return false
+
+        while (startQueue.isNotEmpty() && targetQueue.isNotEmpty())
+        {
+            val currStart = startQueue.remove()
+            val currentTarget = targetQueue.remove()
+            if(currentTarget!=currStart)
+                return false
+        }
+
+        return true
     }
 }
