073f2950cc7434f4feadc8b9870c0feb464e34e8
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-06 11:55:01 +0100

    [ACCEPTED][Problem 2554] Maximum Number of Integers to Choose From a Range I

--- src/medium/MaximumNumberOfIntegersToChooseFromRangeI2554.kt
+++ src/medium/MaximumNumberOfIntegersToChooseFromRangeI2554.kt
@@ -2,21 +2,21 @@ package medium
 
 object MaximumNumberOfIntegersToChooseFromRangeI2554 {
     fun maxCount(banned: IntArray, n: Int, maxSum: Int): Int {
-        val bannedNumbersInRange = mutableSetOf<Int>()
+        val bannedSet = mutableSetOf<Int>()
 
         // Keep the banned numbers that are less than n in a set
         for (num in banned) {
-            if (num <= n)
-                bannedNumbersInRange.add(num)
+            bannedSet.add(num)
         }
 
         // Loop over the numbers from 1 to n and if the number is not banned, use it
         var sum = 0
-        var maxCount=0
-        for(i in 1 ..n)
-        {
-            if(sum+i<maxSum && bannedNumbersInRange.contains(i).not()){
-                sum+=i
+        var maxCount = 0
+        for (i in 1..n) {
+            if(bannedSet.contains(i)) continue
+
+            if (sum + i <= maxSum) {
+                sum += i
                 maxCount++
             }
 
