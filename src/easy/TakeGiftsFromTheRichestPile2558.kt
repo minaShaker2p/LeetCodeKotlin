545e8c231b912615011f1bb7edece64ded86ba2d
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-12 09:42:02 +0100

    [NOT ACCEPTED][Problem 2558] Take Gifts From the Richest Pile

--- src/easy/TakeGiftsFromTheRichestPile2558.kt
+++ src/easy/TakeGiftsFromTheRichestPile2558.kt
@@ -2,6 +2,20 @@ package easy
 
 object TakeGiftsFromTheRichestPile2558 {
     fun pickGifts(gifts: IntArray, k: Int): Long {
-        return 0
+
+        (1..k).forEach {
+            // Find the maximum pile of gifts
+            var maxPile = Int.MIN_VALUE
+            var maxPileIndex = -1
+            for (i in gifts.indices) {
+                if (gifts[i] > maxPile) {
+                    maxPile = gifts[i]
+                    maxPileIndex = i
+                }
+            }
+            val maxPileSquareRoot = Math.sqrt(gifts[maxPileIndex].toDouble()).toInt()
+            gifts[maxPileIndex] = maxPileSquareRoot
+        }
+        return gifts.sum().toLong()
     }
 }
