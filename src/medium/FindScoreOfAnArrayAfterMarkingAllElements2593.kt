7d1ab2c8fc3c8f27ce9c7d253893a0c84bb754d6
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-13 12:13:05 +0100

    [ACCEPTED][Problem 2593] Find Score of an Array After Marking All Elements

--- src/medium/FindScoreOfAnArrayAfterMarkingAllElements2593.kt
+++ src/medium/FindScoreOfAnArrayAfterMarkingAllElements2593.kt
@@ -2,37 +2,35 @@ package medium
 
 object FindScoreOfAnArrayAfterMarkingAllElements2593 {
     fun findScore(nums: IntArray): Long {
-        //  Keep track marked nums
-        //  unmarked -> false
-        //  marked -> true
-        val markedNum = BooleanArray(nums.size) { false }
-
         var score = 0L
+        val sorted = Array(nums.size) { IntArray(2) }
+        val marked = BooleanArray(nums.size)
 
-        // Iterate over the array until all items are marked
-        while (markedNum.isAllItemsMarked().not()) {
-            // Iterate over the array items to find the smallest unmarked number
-            var smallestNumIndex = -1
-            var smallestNum = Int.MAX_VALUE
-            for (i in nums.indices) {
-                if (markedNum[i].not() && nums[i] < smallestNum) {
-                    smallestNum = nums[i]
-                    smallestNumIndex = i
-                }
-            }
-            markedNum[smallestNumIndex] =true
-            if (smallestNumIndex > 0)
-                markedNum[smallestNumIndex - 1] = true
-            if (smallestNumIndex < nums.size - 1)
-                markedNum[smallestNumIndex + 1] = true
-            score += smallestNum
+        for (i in nums.indices) {
+            sorted[i][0] = nums[i]
+            sorted[i][1] = i
         }
 
+        // sort the array
+        sorted.sortWith(comparator = { arr1, arr2 -> arr1[0] - arr2[0] })
+
+        for (i in nums.indices) {
+            val number = sorted[i][0]
+            val index = sorted[i][1]
+            if (marked[index].not()) {
+                score += number
+                marked[index]=true
+                // mark adjacent elements if they exist
+                if (index - 1 >= 0) {
+                    marked[index - 1] = true
+                }
+                if (index + 1 < nums.size) {
+                    marked[index + 1] = true
+                }
+            }
+
+        }
         return score
     }
 
-    // Extension function to check if the all array elements are marked
-    private fun BooleanArray.isAllItemsMarked(): Boolean {
-        return this.all { it }
-    }
 }
