d20540f10442bf43762f40a09fa0dd0e444558e6
Mina Rezkalla <minashakeralfy@gmail.com>
2024-12-16 11:22:11 +0100

    [ACCEPTED][Problem 3264] Final Array State After K Multiplication Operations I

--- src/easy/FinalArrayStateAfterKMultiplicationOperationsI3264.kt
+++ src/easy/FinalArrayStateAfterKMultiplicationOperationsI3264.kt
@@ -2,6 +2,18 @@ package easy
 
 object FinalArrayStateAfterKMultiplicationOperationsI3264 {
     fun getFinalState(nums: IntArray, k: Int, multiplier: Int): IntArray {
+        // Iterate over the array k times
+        (0 until k).forEach { i ->
 
+            // find the minimum element in the array
+            var minimumElementIndex = 0
+            for (j in nums.indices) {
+                if (nums[j] < nums[minimumElementIndex])
+                    minimumElementIndex = j
+            }
+            nums[minimumElementIndex] = nums[minimumElementIndex] * multiplier
+        }
+
+        return nums
     }
 }
