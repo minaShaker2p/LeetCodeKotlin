935e225680b4be3e4789b141079c3c648933c41d
Mina Rezkalla <minashakeralfy@gmail.com>
2025-01-16 09:01:54 +0100

    [Memory Limit Exceeded][Problem 2425] Bitwise XOR of All Pairings

--- src/medium/BitwiseXOROfAllPairings2425.kt
+++ src/medium/BitwiseXOROfAllPairings2425.kt
@@ -2,6 +2,19 @@ package medium
 
 object BitwiseXOROfAllPairings2425 {
     fun xorAllNums(nums1: IntArray, nums2: IntArray): Int {
-        return 0
+        val pairsList = mutableListOf<Int>()
+
+        for (n1 in nums1) {
+            for (n2 in nums2) {
+                val xorPair = n1 xor n2
+                pairsList.add(xorPair)
+            }
+        }
+        var result = pairsList[0]
+        for (i in 1 until pairsList.size)
+        {
+            result = result xor pairsList[i]
+        }
+        return result
     }
 }
