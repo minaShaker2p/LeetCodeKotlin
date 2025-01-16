4638472748d3b58366e0aef6f79fae2fc3fa5afd
Mina Rezkalla <minashakeralfy@gmail.com>
2025-01-16 09:23:40 +0100

    [ACCEPTED][Problem 2425] Bitwise XOR of All Pairings

--- src/medium/BitwiseXOROfAllPairings2425.kt
+++ src/medium/BitwiseXOROfAllPairings2425.kt
@@ -2,19 +2,36 @@ package medium
 
 object BitwiseXOROfAllPairings2425 {
     fun xorAllNums(nums1: IntArray, nums2: IntArray): Int {
-        val pairsList = mutableListOf<Int>()
+     // Get length of Arrays
+        val len1= nums1.size
+        val len2 = nums2.size
 
-        for (n1 in nums1) {
-            for (n2 in nums2) {
-                val xorPair = n1 xor n2
-                pairsList.add(xorPair)
+        // Map to store frequency of each number
+        val freq = HashMap<Int,Int>()
+
+        // Add frequencies for num1 elements
+        // Each element appear n2 times in final result
+        for(num in nums1)
+        {
+            freq[num]=freq.getOrDefault(num,0)+len2
+        }
+
+        // Add frequencies for num2 elements
+        // Each element appear n1 times in final result
+        for(num in nums2)
+        {
+            freq[num]=freq.getOrDefault(num,0)+len1
+        }
+
+        // XOR numbers that appear odd number of times
+        var ans = 0
+        for((k,v) in freq)
+        {
+            if(v%2==1)
+            {
+                ans = ans xor k
             }
         }
-        var result = pairsList[0]
-        for (i in 1 until pairsList.size)
-        {
-            result = result xor pairsList[i]
-        }
-        return result
+        return ans
     }
 }
