0dccca966f4a47046f8423f84b9d27e1155c1802
Mina Rezkalla <minashakeralfy@gmail.com>
2025-01-21 09:53:56 +0100

    [ACCEPTED][Problem 2017] Grid Game

--- src/medium/GridGame2017.kt
+++ src/medium/GridGame2017.kt
@@ -2,6 +2,21 @@ package medium
 
 object GridGame2017 {
     fun gridGame(grid: Array<IntArray>): Long {
+        // Calculate the sun of all element for the first row
+        var firstRowSum = 0L
+        grid[0].forEach { num ->
+            firstRowSum += num
+        }
 
+        var secondRowSum = 0L
+        var minimumSum = Long.MAX_VALUE
+
+        for (turnIndex in grid[0].indices) {
+            firstRowSum -= grid[0][turnIndex]
+            // Find the minimum value out of firstRowSum and secondRowSum
+            minimumSum = Math.min(minimumSum, Math.max(firstRowSum, secondRowSum))
+            secondRowSum += grid[1][turnIndex]
+        }
+        return minimumSum
     }
 }
