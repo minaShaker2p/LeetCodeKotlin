30f1739887ab6cae2a79f54413060f34a13f2925
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-05 20:32:21 +0100

    [UNACCEPTED][Problem 1790] Check if One String Swap Can Make Strings Equal

--- src/easy/CheckIfOneStringSwapCanMakeStringsEqual1790.kt
+++ src/easy/CheckIfOneStringSwapCanMakeStringsEqual1790.kt
@@ -2,6 +2,20 @@ package easy
 
 object CheckIfOneStringSwapCanMakeStringsEqual1790 {
     fun areAlmostEqual(s1: String, s2: String): Boolean {
+        val map = HashMap<Char, Int>()
+
+        s1.forEach { char ->
+            map[char] = map.getOrDefault(char, 0) + 1
+        }
+
+        s2.forEach { char ->
+            map[char] = map.getOrDefault(char, 0) + 1
+        }
+
+        for ((k, v) in map) {
+            if (v % 2 != 0)
+                return false
+        }
 
         return true
     }
