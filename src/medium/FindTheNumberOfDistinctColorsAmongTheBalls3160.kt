891a899f3b70f14ebab229a4e54bcd07b1f7de49
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-07 18:22:47 +0100

    [UNACCEPTED][Problem 3160] Find the Number of Distinct Colors Among the Balls

--- src/medium/FindTheNumberOfDistinctColorsAmongTheBalls3160.kt
+++ src/medium/FindTheNumberOfDistinctColorsAmongTheBalls3160.kt
@@ -2,6 +2,24 @@ package medium
 
 object FindTheNumberOfDistinctColorsAmongTheBalls3160 {
     fun queryResults(limit: Int, queries: Array<IntArray>): IntArray {
+        val balls = IntArray(limit + 1) { -1 }
 
+        for (query in queries) {
+            balls[query[0]] = query[1]
+        }
+
+        val colorFreq = HashMap<Int, Int>()
+
+        for (ball in balls) {
+            colorFreq[ball] = colorFreq.getOrDefault(ball, 0) + 1
+        }
+
+        val result = mutableListOf<Int>()
+        for (i in balls.indices)
+        {
+            if(colorFreq.getOrDefault(balls[i],0)==1)
+                result.add(i)
+        }
+        return result.toIntArray()
     }
 }
