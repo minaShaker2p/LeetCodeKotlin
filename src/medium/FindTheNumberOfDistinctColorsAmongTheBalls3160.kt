71cdc30819f5b34814cced527620c19f26ef6306
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-07 22:31:44 +0100

    [Memory Limit Exceeded][Problem 3160] Find the Number of Distinct Colors Among the Balls

--- src/medium/FindTheNumberOfDistinctColorsAmongTheBalls3160.kt
+++ src/medium/FindTheNumberOfDistinctColorsAmongTheBalls3160.kt
@@ -2,24 +2,36 @@ package medium
 
 object FindTheNumberOfDistinctColorsAmongTheBalls3160 {
     fun queryResults(limit: Int, queries: Array<IntArray>): IntArray {
-        val balls = IntArray(limit + 1) { -1 }
+        val n = queries.size
+        val result = IntArray(n)
+        val colorMap = HashMap<Int, Int>()
+        val ballArray = IntArray(limit + 1)
 
-        for (query in queries) {
-            balls[query[0]] = query[1]
+        // Iterate through queries
+        for (i in 0 until n) {
+            // Extract ball label and color from the query
+            val ball = queries[i][0]
+            val color = queries[i][1]
+
+            // Check if the ball is already colored
+            if (ballArray[ball] != 0) {
+                // Decrement the count of the color o the ball
+                val prevColor = ballArray[ball]
+                colorMap[prevColor] = colorMap.getOrDefault(prevColor, 0) - 1
+
+                // If there are no balls with the previous color left, remove the color from the map
+                if (colorMap.getOrDefault(prevColor, 0) == 0) {
+                    colorMap.remove(prevColor)
+                }
+            }
+            // Set color of ball to the new color
+            ballArray[ball] = color
+
+            // Increment the count of the new color
+            colorMap[color] = colorMap.getOrDefault(color, 0) + 1
+
+            result[i]=colorMap.size
         }
-
-        val colorFreq = HashMap<Int, Int>()
-
-        for (ball in balls) {
-            colorFreq[ball] = colorFreq.getOrDefault(ball, 0) + 1
-        }
-
-        val result = mutableListOf<Int>()
-        for (i in balls.indices)
-        {
-            if(colorFreq.getOrDefault(balls[i],0)==1)
-                result.add(i)
-        }
-        return result.toIntArray()
+        return result
     }
 }
