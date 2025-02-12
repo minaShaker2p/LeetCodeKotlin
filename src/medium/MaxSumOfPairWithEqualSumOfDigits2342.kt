cf1a6043dd467be6973aa9dfb44b4b31fc3ebc73
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-12 21:41:49 +0100

    [ACCEPTED][Problem2342] Max Sum of a Pair With Equal Sum of Digits

--- src/medium/MaxSumOfPairWithEqualSumOfDigits2342.kt
+++ src/medium/MaxSumOfPairWithEqualSumOfDigits2342.kt
@@ -2,31 +2,34 @@ package medium
 
 object MaxSumOfPairWithEqualSumOfDigits2342 {
     fun maximumSum(nums: IntArray): Int {
-        val sumToIndices = HashMap<Int, MutableList<Int>>()
+        val digitSumPairs = Array(nums.size) { Pair(0, 0) }
 
-        nums.forEachIndexed { index, num ->
-            val digitSum = calculateDigitSum(num)
-            val indicesList = sumToIndices.getOrDefault(digitSum, mutableListOf())
-            indicesList.add(index)
-            sumToIndices[digitSum] = indicesList
+        // Store numbers with their digits sums as pairs
+        for (i in nums.indices) {
+            val digitSum = calculateDigitSum(nums[i])
+            digitSumPairs[i] = Pair(digitSum, nums[i])
         }
 
-        var result = -1
+        // Sort based on digit sums, and if equal, by number value
+        val sortedDigitSumPairs =digitSumPairs.sortedWith(compareBy({ it.first }, { it.second }))
 
-        for ((_, v) in sumToIndices) {
-            if (v.size >= 2) {
-                for (i in 0 until v.size - 1) {
-                    for (j in i + 1 until v.size) {
-                        result = Math.max(result, nums[v[i]] + nums[v[j]])
-                    }
-                }
+        var maxPairSum = -1
 
+        // Iterate through the sorted array to find the maximum sum of pairs
+        for (i in 1 until sortedDigitSumPairs.size) {
+            val currentDigitSum = sortedDigitSumPairs[i].first
+            val previousDigitSum = sortedDigitSumPairs[i - 1].first
+
+            // If two consecutive number have the same digit sum
+            if (currentDigitSum == previousDigitSum) {
+                val currentSum = sortedDigitSumPairs[i].second + sortedDigitSumPairs[i - 1].second
+                maxPairSum = Math.max(maxPairSum, currentSum)
             }
         }
-
-        return result
+        return maxPairSum
     }
 
+    // Helper function to compute the sum of digits of a number
     private fun calculateDigitSum(num: Int): Int {
         var digitSum = 0
         var tempNum = num
