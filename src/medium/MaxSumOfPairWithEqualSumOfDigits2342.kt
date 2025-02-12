9406d61c358904537bd3e15bec1816e49159bb4b
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-12 20:58:04 +0100

    [ACCEPTED][Problem2342] Max Sum of a Pair With Equal Sum of Digits

--- src/medium/MaxSumOfPairWithEqualSumOfDigits2342.kt
+++ src/medium/MaxSumOfPairWithEqualSumOfDigits2342.kt
@@ -2,6 +2,33 @@ package medium
 
 object MaxSumOfPairWithEqualSumOfDigits2342 {
     fun maximumSum(nums: IntArray): Int {
-        return 0
+        val sumToIndices = HashMap<Int, MutableList<Int>>()
+
+        nums.forEachIndexed { index, _ ->
+            var num = nums[index]
+            var sumOfDigits = 0
+            while (num != 0) {
+                sumOfDigits += num % 10
+                num /= 10
+            }
+            val indicesList = sumToIndices.getOrDefault(sumOfDigits, mutableListOf())
+            indicesList.add(index)
+            sumToIndices[sumOfDigits] = indicesList
+        }
+
+        var result = -1
+
+        for ((_, v) in sumToIndices) {
+            if (v.size >= 2) {
+                for (i in 0 until v.size - 1) {
+                    for (j in i + 1 until v.size) {
+                        result = Math.max(result, nums[v[i]] + nums[v[j]])
+                    }
+                }
+
+            }
+        }
+
+        return result
     }
 }
