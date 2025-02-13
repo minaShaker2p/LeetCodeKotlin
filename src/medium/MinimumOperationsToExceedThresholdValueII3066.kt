8111dade77657522b0c594d165f3d0ee33a7a23f
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-13 21:38:33 +0100

    [NOT ACCEPTED][Problem 3066] Minimum Operations to Exceed Threshold Value II

--- src/medium/MinimumOperationsToExceedThresholdValueII3066.kt
+++ src/medium/MinimumOperationsToExceedThresholdValueII3066.kt
@@ -1,7 +1,32 @@
 package medium
 
+import java.util.LinkedList
+import java.util.PriorityQueue
+import java.util.Queue
+
 object MinimumOperationsToExceedThresholdValueII3066 {
     fun minOperations(nums: IntArray, k: Int): Int {
+        val minHeap: PriorityQueue<Int> = PriorityQueue<Int>()
+
+        nums.forEach { num ->
+            minHeap.add(num)
+        }
+
+        var numOfOperations = 0
+
+        while (minHeap.size >= 2) {
+            val peek = minHeap.remove()
+            if (peek >= k)
+                break
+
+            val secondSmallestValue = minHeap.remove()
+
+            val current = (peek *2)+secondSmallestValue
+            minHeap.add(current)
+            numOfOperations++
+        }
+
+        return numOfOperations
 
     }
 }
