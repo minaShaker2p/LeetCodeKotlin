cd632ddeee7b84b4565203336057705eed7666b3
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-25 21:59:18 +0100

    [ACCEPTED][Problem 1524] Number of Sub-arrays With Odd Sum

--- src/medium/NumberOfSubArraysWithOddSum1524.kt
+++ src/medium/NumberOfSubArraysWithOddSum1524.kt
@@ -2,17 +2,24 @@ package medium
 
 object NumberOfSubArraysWithOddSum1524 {
     fun numOfSubarrays(arr: IntArray): Int {
-        val MOD = 1000000007
-        var oddSumCount = 0
-        for (i in arr.indices) {
-            var count = 0
-            for (j in i..<arr.size) {
-                count += arr[j]
-                if (count % 2 == 1)
-                    oddSumCount++
+        val MOD = 1_000_000_007
+        var oddCount = 0
+        var evenCount = 1
+        var count = 0
+        var prefixSum = 0
+        for (num in arr) {
+            prefixSum += num
+            // if it is  odd prefix sum, add even count to the count
+            if (prefixSum % 2 == 1) {
+                count += evenCount
+                oddCount++
+            } else {
+                count += oddCount
+                evenCount++
             }
+            count %=MOD
         }
 
-        return (oddSumCount % MOD)
+        return count
     }
 }
