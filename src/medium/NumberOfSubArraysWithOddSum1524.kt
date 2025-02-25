a5d15d249777b75039cadfa772a6d0759b548ea0
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-25 21:37:44 +0100

    [Time Limit Exceeded][Problem 1524] Number of Sub-arrays With Odd Sum

--- src/medium/NumberOfSubArraysWithOddSum1524.kt
+++ src/medium/NumberOfSubArraysWithOddSum1524.kt
@@ -2,23 +2,17 @@ package medium
 
 object NumberOfSubArraysWithOddSum1524 {
     fun numOfSubarrays(arr: IntArray): Int {
-        // Calculate all sub arrays
-        val subArrays = mutableListOf<List<Int>>()
+        val MOD = 1000000007
+        var oddSumCount = 0
         for (i in arr.indices) {
-            for (j in i + 1..arr.size) {
-                subArrays.add(arr.copyOfRange(i, j ).toList())
+            var count = 0
+            for (j in i..<arr.size) {
+                count += arr[j]
+                if (count % 2 == 1)
+                    oddSumCount++
             }
         }
 
-        // Calculate the sum of each sub array
-        var oddSumCount = 0L
-        for(subArray in subArrays)
-        {
-            if(subArray.sum()%2==1)
-                oddSumCount++
-        }
-
-        // Return number of odd  sum sub array
-        return  (oddSumCount % 1000000007).toInt()
+        return (oddSumCount % MOD)
     }
 }
