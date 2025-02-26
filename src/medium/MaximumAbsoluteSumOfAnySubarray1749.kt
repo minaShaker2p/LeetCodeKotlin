2474d6c1a7d7e1eae25b23f65813bfbea9c5a0dc
Mina Rezkalla <minashakeralfy@gmail.com>
2025-02-26 21:00:12 +0100

    [ACCEPTED][Problem 1749] Maximum Absolute Sum of Any Subarray

--- src/medium/MaximumAbsoluteSumOfAnySubarray1749.kt
+++ src/medium/MaximumAbsoluteSumOfAnySubarray1749.kt
@@ -5,19 +5,27 @@ import kotlin.math.abs
 object MaximumAbsoluteSumOfAnySubarray1749 {
 
     fun maxAbsoluteSum(nums: IntArray): Int {
-        val n = nums.size
-
-        var maxAbsSum = Int.MIN_VALUE
+        var minPrefixSum = Int.MAX_VALUE
+        var maxPrefixSum = Int.MIN_VALUE
+        var prefixSum = 0
+        var maxAbsSum = 0
 
         for (i in nums.indices) {
-            var counter = 0
-            for (j in i..<n) {
-                counter += nums[j]
-                maxAbsSum = maxAbsSum.coerceAtLeast(abs(counter))
+            // Prefix sum from index 0 to i
+            prefixSum += nums[i]
 
+            // Minimum & Maximum prefix sum we have seen so far
+            minPrefixSum = minPrefixSum.coerceAtMost(prefixSum)
+            maxPrefixSum = maxPrefixSum.coerceAtLeast(prefixSum)
+
+            if (prefixSum > 0) {
+                // If the prefixSum is positive, we will get the difference between prefixSum & minPrefixSum
+                maxAbsSum = maxAbsSum.coerceAtLeast(prefixSum.coerceAtLeast(prefixSum - minPrefixSum))
+            } else {
+                // If the prefixSum is negative, we will get the absolute difference between prefixSum & maxPrefixSum
+                maxAbsSum = maxAbsSum.coerceAtLeast(abs(prefixSum).coerceAtLeast(abs(prefixSum - maxPrefixSum)))
             }
         }
         return maxAbsSum
-
     }
 }
