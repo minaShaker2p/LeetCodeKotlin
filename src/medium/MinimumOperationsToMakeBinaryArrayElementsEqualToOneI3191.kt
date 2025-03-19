22afcfd4fd74acbd17e9787fe71659bc6ada17e3
Mina Rezkalla <minashakeralfy@gmail.com>
2025-03-19 08:40:10 +0100

    [ACCEPTED][Problem 3191] Minimum Operations to Make Binary Array Elements Equal to One I

--- src/medium/MinimumOperationsToMakeBinaryArrayElementsEqualToOneI3191.kt
+++ src/medium/MinimumOperationsToMakeBinaryArrayElementsEqualToOneI3191.kt
@@ -2,6 +2,19 @@ package medium
 
 object MinimumOperationsToMakeBinaryArrayElementsEqualToOneI3191 {
     fun minOperations(nums: IntArray): Int {
+        // initialize count to track the number of flip operations
+        var count = 0
 
+        // Itarate through num starting from 2nd element
+        for (i in 2 until nums.size) {
+            if (nums[i - 2] == 0) {
+                count++
+                nums[i - 2] = nums[i - 2].xor(1)
+                nums[i - 1] = nums[i - 1].xor(1)
+                nums[i] = nums[i].xor(1)
+            }
+        }
+
+        return if (nums.sum() == nums.size) count else -1
     }
 }
