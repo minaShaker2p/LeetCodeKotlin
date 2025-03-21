a51b55e8ee047885452e859e98f41aed969b9fcf
Mina Rezkalla <minashakeralfy@gmail.com>
2025-03-21 08:14:21 +0100

    [Problem 2115] Find All Possible Recipes from Given Supplies

---
+++ src/medium/FindAllPossibleRecipesFromGivenSupplies2115.kt
@@ -0,0 +1,11 @@
+package medium
+
+object FindAllPossibleRecipesFromGivenSupplies2115 {
+    fun findAllRecipes(
+        recipes: Array<String>,
+        ingredients: List<List<String>>,
+        supplies: Array<String>
+    ): List<String> {
+        return emptyList()
+    }
+}
