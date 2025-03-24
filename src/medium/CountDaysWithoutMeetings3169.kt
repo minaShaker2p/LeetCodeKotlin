0740d7a9fa375ca3fa2705099e5579aa34d71061
Mina Rezkalla <minashakeralfy@gmail.com>
2025-03-24 09:48:49 +0100

    [Time Limit Exceeded][Problem 3169] Count Days Without Meetings

--- src/medium/CountDaysWithoutMeetings3169.kt
+++ src/medium/CountDaysWithoutMeetings3169.kt
@@ -2,6 +2,16 @@ package medium
 
 object CountDaysWithoutMeetings3169 {
     fun countDays(days: Int, meetings: Array<IntArray>): Int {
-        return 0
+        val workingDays = IntArray(days)
+        for (meeting in meetings) {
+            (meeting[0]..meeting[1]).forEach { workingDays[it-1] = 1 }
+        }
+        var count =0
+        workingDays.forEach {
+            if(it==0)
+                count++
+        }
+
+        return count
     }
 }
