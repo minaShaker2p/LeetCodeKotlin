e2291491eafd50e0958b013d46ad070f714def89
Mina Rezkalla <minashakeralfy@gmail.com>
2025-04-04 09:57:16 +0200

    [ACCEPTED][Problem 1123]Lowest Common Ancestor of Deepest Leaves

--- src/medium/LowestCommonAncestorOfDeepestLeaves1123.kt
+++ src/medium/LowestCommonAncestorOfDeepestLeaves1123.kt
@@ -2,6 +2,25 @@ package medium
 
 object LowestCommonAncestorOfDeepestLeaves1123 {
     fun lcaDeepestLeaves(root: TreeNode?): TreeNode? {
+        return dfs(root).first
+    }
 
+    private fun dfs(root: TreeNode?): Pair<TreeNode?,Int>{
+        if(root == null)
+        {
+            return Pair(null,0)
+        }
+
+        val left = dfs(root.left)
+        val right = dfs(root.right)
+
+        if(left.second> right.second)
+        {
+            return  Pair(left.first,left.second+1)
+        }
+        if(left.second < right.second){
+            return Pair(right.first,right.second+1)
+        }
+        return Pair(root,left.second+1)
     }
 }
