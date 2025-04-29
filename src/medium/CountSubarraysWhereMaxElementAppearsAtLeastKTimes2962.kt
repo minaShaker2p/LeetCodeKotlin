a2037a4e48e676fc67238107b2a2edc20f3f774d
Mina Rezkalla <minashakeralfy@gmail.com>
2025-04-29 18:55:05 +0200

    [Time Limit Exceeded] Count Subarrays Where Max Element Appears at Least K Times

--- src/medium/CountSubarraysWhereMaxElementAppearsAtLeastKTimes2962.kt
+++ src/medium/CountSubarraysWhereMaxElementAppearsAtLeastKTimes2962.kt
@@ -2,6 +2,33 @@ package medium
 
 object CountSubarraysWhereMaxElementAppearsAtLeastKTimes2962 {
     fun countSubarrays(nums: IntArray, k: Int): Long {
+        val maximumNum = nums.max()
+        val n = nums.size
+        var left = 0
+        var right = 0
+        var subArrayCount = 0L
 
+        while (left < n) {
+            while (right < n) {
+                val subArraySize = (right - left) + 1
+                if (subArraySize < k) {
+                    right++
+                    continue
+                }
+
+                val subArray = nums.copyOfRange(left, right + 1)
+
+                if (subArray.IsArrayMaxElementAppearAtLeastKTimes(maximumNum, k))
+                    subArrayCount++
+                right++
+            }
+            left++
+            right = left
+        }
+        return subArrayCount
+    }
+
+    fun IntArray.IsArrayMaxElementAppearAtLeastKTimes(max: Int, k: Int): Boolean {
+        return this.count { it == max } >= k
     }
 }
