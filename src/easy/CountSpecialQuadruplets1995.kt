2654770924eee4aa6f959686829d1eb57c2011a7
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-01 14:45:24 +0200

    [ACCEPTED][Problem 1995] Count Special Quadruplets

--- src/easy/CountSpecialQuadruplets1995.kt
+++ src/easy/CountSpecialQuadruplets1995.kt
@@ -2,7 +2,21 @@ package easy
 
 object CountSpecialQuadruplets1995 {
     fun countQuadruplets(nums: IntArray): Int {
+        val n = nums.size
+        var count = 0
 
-        return 0
+        for (i in 0 until n - 3) {
+            for (j in i + 1 until n - 2) {
+                for (k in j + 1 until n - 1) {
+                    for (l in k + 1 until n) {
+                        val sum = nums[i] + nums[j] + nums[k]
+                        if (sum == nums[l])
+                            count++
+                    }
+                }
+            }
+        }
+
+        return count
     }
 }
