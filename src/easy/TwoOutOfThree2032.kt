f16a058ab421b5eff79626e06234bc85248a9159
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-01 22:56:47 +0200

    [ACCEPTED][Problem 2032] Two Out of Three

--- src/easy/TwoOutOfThree2032.kt
+++ src/easy/TwoOutOfThree2032.kt
@@ -2,6 +2,25 @@ package easy
 
 object TwoOutOfThree2032 {
     fun twoOutOfThree(nums1: IntArray, nums2: IntArray, nums3: IntArray): List<Int> {
-        return emptyList()
+        val freqMap = HashMap<Int, Int>()
+
+        nums1.distinct().forEach { num ->
+            freqMap[num] = freqMap.getOrDefault(num, 0) + 1
+        }
+
+        nums2.distinct().forEach { num ->
+            freqMap[num] = freqMap.getOrDefault(num, 0) + 1
+        }
+
+        nums3.distinct().forEach { num ->
+            freqMap[num] = freqMap.getOrDefault(num, 0) + 1
+        }
+
+        val result = mutableListOf<Int>()
+        for ((k, v) in freqMap) {
+            if (v >= 2)
+                result.add(k)
+        }
+        return result
     }
 }
