46450baba062f3cf2134c45badfeaa7d251e5c50
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-01 22:38:45 +0200

    [ACCEPTED][Problem 1995] Count Special Quadruplets

--- src/easy/CountSpecialQuadruplets1995.kt
+++ src/easy/CountSpecialQuadruplets1995.kt
@@ -1,6 +1,8 @@
 package easy
 
 object CountSpecialQuadruplets1995 {
+    //1.brute force
+    //Time: O(N^4); Space: O(1)
     fun countQuadruplets(nums: IntArray): Int {
         val n = nums.size
         var count = 0
@@ -17,6 +19,28 @@ object CountSpecialQuadruplets1995 {
             }
         }
 
+        return count
+    }
+
+    fun countQuadrupletsSOL2(nums: IntArray): Int {
+        val freqMap = HashMap<Int,Int>()
+        val n =  nums.size
+        var count = 0
+
+        for(i in n-2 downTo  1)
+        {
+            for (j in i+1 until n)
+            {
+                val sum = nums[j]-nums[i]
+                freqMap[sum]=freqMap.getOrDefault(sum,0)+1
+            }
+
+            for (j in i-2 downTo  0)
+            {
+                val sum = nums[j]+nums[i-1]
+                count+=freqMap.getOrDefault(sum,0)
+            }
+        }
         return count
     }
 }
