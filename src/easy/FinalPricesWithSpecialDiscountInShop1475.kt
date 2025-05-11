287b4a313f8584e70a87066a146a9eff9800e41a
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-11 20:36:56 +0200

    [ACCEPTED][Problem 1475]Final Prices With a Special Discount in a Shop

--- src/easy/FinalPricesWithSpecialDiscountInShop1475.kt
+++ src/easy/FinalPricesWithSpecialDiscountInShop1475.kt
@@ -1,6 +1,16 @@
 package easy
+
 object FinalPricesWithSpecialDiscountInShop1475 {
     fun finalPrices(prices: IntArray): IntArray {
 
+        for (i in 0 until prices.size - 1) {
+            for (j in i + 1 until prices.size) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] = prices[i] - prices[j]
+                    break
+                }
+            }
+        }
+        return prices
     }
 }
