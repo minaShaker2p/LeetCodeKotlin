0615791d2ca0bce2f5c1722736fcbfd69bc1129c
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-11 20:17:38 +0200

    [ACCEPTED][Problem 1550] Three Consecutive Odds

--- src/easy/ThreeConsecutiveOdds1550.kt
+++ src/easy/ThreeConsecutiveOdds1550.kt
@@ -2,6 +2,33 @@ package easy
 
 object ThreeConsecutiveOdds1550 {
     fun threeConsecutiveOdds(arr: IntArray): Boolean {
+        var odds = 0
+        var previousOdd = -1
+        arr.forEach { num ->
+            when {
+                num.issOdd() && previousOdd.issOdd() -> {
+                    previousOdd = num
+                    odds++
+                }
+
+                num % 2 == 1 -> {
+                    odds = 1
+                    previousOdd = num
+                }
+
+                else -> {
+                    odds = 0
+                    previousOdd = -1
+                }
+            }
+
+            if (odds == 3)
+                return true
+
+        }
+
         return false
     }
+
+    private fun Int.issOdd() = this % 2 == 1
 }
