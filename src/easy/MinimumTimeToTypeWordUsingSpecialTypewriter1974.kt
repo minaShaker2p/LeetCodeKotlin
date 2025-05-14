ccf07f54c4ba5cb70baf5643e08c12c737756caa
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-14 22:32:27 +0200

    [ACCEPTED][Problem 1974] Minimum Time to Type Word Using Special Typewriter

--- src/easy/MinimumTimeToTypeWordUsingSpecialTypewriter1974.kt
+++ src/easy/MinimumTimeToTypeWordUsingSpecialTypewriter1974.kt
@@ -2,6 +2,26 @@ package easy
 
 object MinimumTimeToTypeWordUsingSpecialTypewriter1974 {
     fun minTimeToType(word: String): Int {
+        var minTime = word.length
+        minTime += clockwiseDistance(word[0], 'a').coerceAtMost(counterClockwiseDistance(word[0], 'a'))
 
+        for (i in 1 until word.length)
+        {
+            val clockwise = clockwiseDistance(word[i],word[i-1])
+            val counterClockwise = counterClockwiseDistance(word[i],word[i-1])
+            minTime+= clockwise.coerceAtMost(counterClockwise)
+        }
+        return minTime
+    }
+
+    private fun counterClockwiseDistance(from: Char, to: Char): Int {
+        val start = from.lowercaseChar() - 'a'
+        val end = to.lowercaseChar() - 'a'
+        return (start - end + 26) % 26
+    }
+    private fun clockwiseDistance(from: Char, to: Char): Int {
+        val start = from.lowercaseChar() - 'a'
+        val end = to.lowercaseChar() - 'a'
+        return (end - start + 26) % 26
     }
 }
