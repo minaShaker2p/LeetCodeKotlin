99cc83ca7acc4eb2bc62823ad32f4121542f4d1e
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-19 21:55:33 +0200

    [ACCEPTED][Problem 3024] Type of Triangle

--- src/easy/TypeOfTriangle3024.kt
+++ src/easy/TypeOfTriangle3024.kt
@@ -2,8 +2,27 @@ package easy
 
 object TypeOfTriangle3024 {
 
-    fun triangleType(nums: IntArray): String {
+    // Approach :Mathematics
+    /**
+     * First, sort nums in ascending order, then make the following checks in sequence:
+     *
+     * If nums[0] + nums[1] <= nums[2], return none
+     * If nums[0] = nums[2], return "equilateral"
+     * If nums[0] = nums[1] or nums[1] = nums[2], return "isosceles"
+     * If none of the above conditions are met, return "scalene"
+     */
 
-        return ""
+    fun triangleType(nums: IntArray): String {
+        nums.sort()
+
+        return if (nums[0] + nums[1] <= nums[2]) {
+            "none"
+        } else if (nums[0] == nums[2]) {
+            "equilateral"
+        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
+            "isosceles"
+        } else {
+            "scalene"
+        }
     }
 }
