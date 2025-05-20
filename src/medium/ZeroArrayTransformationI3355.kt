be6065275d0357fd37694018e891a8b288ec4a5b
Mina Rezkalla <minashakeralfy@gmail.com>
2025-05-20 22:51:42 +0200

    [ACCEPTED][Problem 3355] Zero Array Transformation I

--- src/medium/ZeroArrayTransformationI3355.kt
+++ src/medium/ZeroArrayTransformationI3355.kt
@@ -2,6 +2,28 @@ package medium
 
 object ZeroArrayTransformationI3355 {
     fun isZeroArray(nums: IntArray, queries: Array<IntArray>): Boolean {
-        return false
+        val deltaArray = IntArray(nums.size + 1)
+
+        for (query in queries) {
+            val left = query[0]
+            val right = query[1]
+            deltaArray[left] += 1
+            deltaArray[right + 1] -= 1
+        }
+
+        val operationCounts = IntArray(deltaArray.size)
+        var currentOperations = 0
+
+        for (i in deltaArray.indices) {
+            currentOperations += deltaArray[i]
+            operationCounts[i] = currentOperations
+        }
+
+        for (i in nums.indices) {
+            if (operationCounts[i] < nums[i])
+                return false
+        }
+
+        return true
     }
 }
